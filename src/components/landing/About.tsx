c907b8afdeafa4d9a16149100a1d83b603d3a09f
hoang-10n <hoang816528@gmail.com>
2025-01-21 14:24:06 +1030

    init

---
+++ src/components/landing/About.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import { Statistics } from "./Statistics";
+
+export const About = () => {
+  return (
+    <section
+      id="about"
+      className="container py-24 sm:py-32"
+    >
+      <div className="bg-muted/50 border rounded-lg py-12">
+        <div className="px-6 flex flex-col-reverse md:flex-row gap-8 md:gap-12">
+          <Image
+            src="/landing/pilot.png"
+            alt="pilot.png"
+            className="w-[300px] object-contain rounded-lg"
+          />
+          <div className="bg-green-0 flex flex-col justify-between">
+            <div className="pb-6">
+              <h2 className="text-3xl md:text-4xl font-bold">
+                <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
+                  About{" "}
+                </span>
+                Company
+              </h2>
+              <p className="text-xl text-muted-foreground mt-4 text-justify">
+                Plantelligence was founded with the vision of revolutionizing rural agriculture by integrating artificial intelligence and blockchain technology into everyday farming operations. Our mission is to empower farmers by providing them with advanced, easy-to-use tools that increase productivity, improve sustainability, and ensure financial stability. At Plantelligence, we are committed to using technology to build a better future for farming communities around the world.
+              </p>
+            </div>
+
+            <Statistics />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
